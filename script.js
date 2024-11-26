b61063e292c6ea103b467e3753ec21fd6fe2c1a3
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2024-11-26 22:55:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerButton = document.getElementById('hamburger-button');
-  const navbar = document.getElementById('navbar');
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
 
-  hamburgerButton.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('show');
-    navbar.classList.toggle('show');
+navLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Mencegah perilaku default link
+
+    // Sembunyikan semua section
+    sections.forEach(section => {
+      section.style.display = 'none';
+    });
+
+    // Tampilkan section yang sesuai dengan link yang diklik
+    const targetId = link.getAttribute('href'); // Ambil nilai href (misalnya: #pendidikan)
+    const targetSection = document.querySelector(targetId);
+    targetSection.style.display = 'block';
   });
 });
